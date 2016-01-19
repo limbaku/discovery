e4395768dcd85b7715cac5b0dc6938dc78559483
sperez <sandraperezgalisteo"gmail.com>
2016-01-19 17:22:28 +0000

    Created new class

---
+++ src/main/java/discovery/discover.java
@@ -0,0 +1,29 @@
+package discovery;
+
+
+public class discover {
+
+    String value;
+    String key;
+
+    public discover(String value, String key) {
+        this.value = value;
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
